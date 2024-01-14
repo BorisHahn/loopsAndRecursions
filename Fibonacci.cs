3c73481e5424a0b30b2bb5ef6ea73759fcb9f205
Boris Gaidukevich <Borishahn@yandex.ru>
2024-01-15 00:51:01 +0300

    first commit

---
+++ Fibonacci.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+namespace LoopsAndRecursions
+{
+    internal class Fibonacci
+    {
+        private int _member;
+        private int _count = 0;
+        internal Fibonacci(int member)
+        {
+            _member = member;
+        }
+        public void FibonacciByLoop()
+        {
+            Stopwatch stopwatch = new();
+            int _fib1 = 1;
+            int _fib2 = 1;
+            int _fibSum;
+            stopwatch.Start();
+            while (_count < (_member - 2))
+            {
+                _fibSum = _fib1 + _fib2;
+                _fib1 = _fib2;
+                _fib2 = _fibSum;
+                _count++;
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"Значение числа Фибоначчи для {_member} члена последовательности: {_fib2}");
+            Console.WriteLine($"Время затраченное на нахождение n-го члена последовательности {_member} циклом: ticks: {stopwatch.ElapsedTicks}, ms {stopwatch.ElapsedMilliseconds}");
+        }
+
+        public void FibonacciByRecursion()
+        {
+            Stopwatch stopwatch = new();
+            stopwatch.Start();
+            CalculateFibonacciByRecursion(_member);
+            stopwatch.Stop();
+            Console.WriteLine($"Значение числа Фибоначчи для {_member} члена последовательности: {CalculateFibonacciByRecursion(_member)}");
+            Console.WriteLine($"Время затраченное на нахождение n-го члена последовательности {_member} рекурсией: ticks: {stopwatch.ElapsedTicks}, ms {stopwatch.ElapsedMilliseconds}");
+        }
+
+        private int CalculateFibonacciByRecursion(int count)
+        {
+            if (count == 1 || count == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return CalculateFibonacciByRecursion(count - 1) + CalculateFibonacciByRecursion(count - 2);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LoopsAndRecursions
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int memberFibonacci5 = 5;
+            int memberFibonacci10 = 10;
+            int memberFibonacci20 = 20;
+
+            var fibonacci5 = new Fibonacci(memberFibonacci5);
+            var fibonacci10 = new Fibonacci(memberFibonacci10);
+            var fibonacci20 = new Fibonacci(memberFibonacci20);
+            fibonacci5.FibonacciByLoop();
+            fibonacci5.FibonacciByRecursion();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            fibonacci10.FibonacciByLoop();
+            fibonacci10.FibonacciByRecursion();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+
+            fibonacci20.FibonacciByLoop();
+            fibonacci20.FibonacciByRecursion();
+            Console.WriteLine("-----------------------------------------------------------------------------------------------");
+        }
+    }
+}
